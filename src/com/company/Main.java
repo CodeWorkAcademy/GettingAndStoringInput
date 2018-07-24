3994be7fefba1449afbdf50d71f289853320984a
GBTC406001UR <GBTC406001UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-24 10:01:53 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first initial: ");
+        String firstInitial = sc.next();
+
+        System.out.println("Enter the last name: ");
+        String lastName = sc.next();
+
+        System.out.println("Enter the house number: ");
+        int houseNumber = sc.nextInt();
+
+        System.out.println("Enter the street name: ");
+        String streetName = sc.next();
+
+        System.out.println("Enter the street type: ");
+        String streetType = sc.next();
+
+        System.out.println("Enter the city: ");
+        String city = sc.next();
+
+        System.out.print(firstInitial + " " + lastName + " " + houseNumber + " ");
+        System.out.println(streetName + " " + streetType + " " + city);
+
+        sc.close();
+    }
+}
